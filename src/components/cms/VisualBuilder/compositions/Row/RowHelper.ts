c463bea61e5c9088d7b1dd8b355a73b1cedc090a
Kunal Shetye <kunal.shetye@gmail.com>
2024-10-27 18:16:43 +0100

    tweaked header, add column/row/section helpers. need to check CSS on this

---
+++ src/components/cms/VisualBuilder/compositions/Row/RowHelper.ts
@@ -0,0 +1,54 @@
+import { CompositionStructureNode } from "../../../../../services/graphql/__generated/sdk.ts";
+import { getDictionaryFromDisplaySettings } from "../../../../../services/shared/displaySettingsHelpers.ts";
+
+export function getRowStyles(row: CompositionStructureNode) {
+  const displaySettings = row.displaySettings;
+  const dictionary = getDictionaryFromDisplaySettings(displaySettings);
+  console.log(row.displayTemplateKey);
+
+  // console.log(dictionary);
+
+  enum RowFromClasses {
+    md = "md:flex-row",
+    lg = "lg:flex-row",
+    xl = "xl:flex-row",
+  }
+
+  enum ContentSpacingClasses {
+    small = "gap-2",
+    medium = "gap-4",
+    large = "gap-4 lg:gap-8",
+    xl = "gap-4 lg:gap-24",
+    xxl = "gap-4 lg:gap-72",
+    none = "gap-0",
+  }
+
+  enum JustifyContentClasses {
+    center = "justify-center",
+    end = "justify-end",
+    start = "justify-start",
+  }
+
+  enum AlignContentClasses {
+    center = "content-center",
+    end = "content-end",
+    start = "content-start",
+  }
+
+  enum VerticalSpacingClasses {
+    small = "my-2",
+    medium = "my-4",
+    large = "my-8",
+    verylarge = "lg:my-40 my-20",
+    none = "my-0",
+  }
+
+  let cssClasses = [];
+
+  cssClasses.push(ContentSpacingClasses[dictionary["contentSpacing"]] ?? "");
+  cssClasses.push(JustifyContentClasses[dictionary["justifyContent"]] ?? "");
+  cssClasses.push(AlignContentClasses[dictionary["alignContent"]] ?? "");
+  cssClasses.push(VerticalSpacingClasses[dictionary["verticalSpacing"]] ?? "");
+  cssClasses.push(RowFromClasses[dictionary["showAsRowFrom"]] ?? "");
+  return cssClasses;
+}
--- src/components/cms/VisualBuilder/compositions/Section/SectionHelper.ts
+++ src/components/cms/VisualBuilder/compositions/Section/SectionHelper.ts
@@ -4,21 +4,21 @@ import { getDictionaryFromDisplaySettings } from "../../../../../services/shared
 export function getSectionStyles(grid: CompositionStructureNode): string[] {
   const displaySettings = grid.displaySettings;
   const dictionary = getDictionaryFromDisplaySettings(displaySettings);
-
-  console.log(dictionary);
   let cssClasses: string[] = [];
   switch (grid.displayTemplateKey) {
     case "DefaultGrid":
       switch (dictionary["gridWidth"]) {
         case "default":
+          cssClasses.push("container mx-auto px-8");
+          break;
         case "full":
-          cssClasses.push("max-w-full");
+          cssClasses.push("w-full");
           break;
         case "narrow":
-          cssClasses.push("max-w-xl");
+          cssClasses.push("max-w-3xl w-full mx-auto px-8");
           break;
         case "wide":
-          cssClasses.push("max-w-4xl");
+          cssClasses.push("max-w-7xl w-full mx-auto px-8");
           break;
       }
       switch (dictionary["vSpacing"]) {
@@ -26,10 +26,10 @@ export function getSectionStyles(grid: CompositionStructureNode): string[] {
           cssClasses.push("my-8");
           break;
         case "small":
-          cssClasses.push("my-4");
+          cssClasses.push("py-4 md:py-8 lg:py-12");
           break;
         case "large":
-          cssClasses.push("my-16");
+          cssClasses.push("py-8 md:py-16 lg:py-24");
           break;
       }
       switch (dictionary["sectionColor"]) {
@@ -38,9 +38,6 @@ export function getSectionStyles(grid: CompositionStructureNode): string[] {
           break;
         case "blue":
           cssClasses.push("bg-blue-700");
-          break;
-        case "large":
-          cssClasses.push("my-12");
           break;
       }
       break;
