390f060c5d08c280087ff2e42579f5bdb4a47723
Dan Isaacs <daniel.isaacs@optimizely.com>
2025-06-06 11:17:04 -0600

    multi-adapter setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mkcert from 'vite-plugin-mkcert';
 
 import node from '@astrojs/node';
 import { adapter } from "astro-auto-adapter";
-import type { NetlifyAdapterOptions } from "astro-auto-adapter";
-import type { VercelAdapterOptions } from "astro-auto-adapter";
 
 import alpinejs from '@astrojs/alpinejs';
 import tailwindcss from '@tailwindcss/vite';
