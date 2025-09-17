50d4a224a3ad4373387d77f9d8e3251e51a041cc
tgardner18 <109979067+tgardner18@users.noreply.github.com>
2025-09-17 13:05:24 -0400

    font style changes for Choctaw site

--- astro.config.mjs
+++ astro.config.mjs
@@ -293,6 +293,7 @@ export default defineConfig({
                 provider: fontProviders.google(),
                 name: 'Oswald',
                 cssVariable: '--font-oswald',
+                weights: ['200', '300', '400', '500', '600', '700'],
             },
             {
                 provider: fontProviders.google(),
