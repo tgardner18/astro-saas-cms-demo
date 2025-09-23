89e40eff159ee0b8f75130e369daed25550755d2
tgardner18 <109979067+tgardner18@users.noreply.github.com>
2025-09-23 16:01:51 -0400

    font updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -273,6 +273,7 @@ export default defineConfig({
                 provider: fontProviders.google(),
                 name: 'Montserrat',
                 cssVariable: '--font-montserrat',
+                weights: ['200,300,400,500,600,700,800,900'],
             },
             {
                 provider: fontProviders.google(),
